577c39a31899a18855e7a0c258fd390843d6c35d
Sayonarabay <depalomero@gmail.com>
2026-04-29 20:01:46 +0200

    Delete travel-studio-final directory

--- travel-studio-final/lib/services/index.ts
+++
@@ -1,7 +0,0 @@
-export { searchFlights } from "./kiwi";
-export { searchHotels } from "./booking";
-export { searchHotelsTrivago } from "./trivago";
-export { searchAttractions } from "./tripadvisor";
-export { searchEvents } from "./fever";
-export { fetchProviderBundle, getDestinationsForTypes } from "./aggregator";
-export { generateTrip } from "./anthropic";
